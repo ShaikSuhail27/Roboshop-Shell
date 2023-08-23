87e2a5b3fdd193731c66be70e9023198c84f85ca
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-23 13:08:00 +0530

    MongoDB Installation

--- Mongodb.sh
+++ Mongodb.sh
@@ -17,12 +17,14 @@ Y="\e[33m"
 if [ $USERID -ne 0 ];
 then
 echo -e "$R Error:please install with Root Access $N"
+exit 1
 fi
 
 VALIDATE() {
 if [ $1 -ne 0 ];
 then
 echo -e "$2 is $R Failure $N"
+exit 1
 else
 echo -e "$2 is $G Success $N"
 fi
