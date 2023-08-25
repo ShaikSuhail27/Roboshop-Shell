6b9478a6dd6cc82ef4cd5f1cc8e9d035bc91b006
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-26 02:03:48 +0530

    Creating the Instances and route 53 records

--- Create-ec2.sh
+++ Create-ec2.sh
@@ -4,6 +4,7 @@ SERVER_NAMES=("MongoDB" "Redis" "MYSQL" "RabbitMQ" "Catalogue" "User" "Cart" "Sh
 INSTANCE_TYPE=" "
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GID=sg-05fe3b7791764948d
+DOMAIN_NAME=suhaildevops.online
 
 for i in "${SERVER_NAMES[@]}"
 do
@@ -17,4 +18,16 @@ fi
 echo "Creating the $i Instance"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 echo "Created the $i Instance" :$IP_ADDRESS
+
+$ aws route53 change-resource-record-sets --hosted-zone-id Z0491401N7I6FTW4RMTS --change-batch '{
+            "Comment": "CREATE",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+}}]
+}'
 done
