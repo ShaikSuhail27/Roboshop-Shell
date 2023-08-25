20c36cc35cf7f042035496696795563a49924c6d
Shaiksuhail27 <Shaiksuhail2767@gmail.com>
2023-08-26 02:05:01 +0530

    Creating the Instances and route 53 records

--- Create-ec2.sh
+++ Create-ec2.sh
@@ -19,7 +19,7 @@ echo "Creating the $i Instance"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 echo "Created the $i Instance" :$IP_ADDRESS
 
-$ aws route53 change-resource-record-sets --hosted-zone-id Z0491401N7I6FTW4RMTS --change-batch '{
+aws route53 change-resource-record-sets --hosted-zone-id Z0491401N7I6FTW4RMTS --change-batch '{
             "Comment": "CREATE",
             "Changes": [{
             "Action": "CREATE",
